cfe35f71530a9d58658ea0cf34c902805020fb4a
Tweenkyy-sagg <tweenkyy_4@mail.ru>
2025-10-06 12:16:29 +0300

    lab5..c

--- lab5..c
+++ lab5..c
@@ -1,21 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
 
 int main() {
-    setlocale(LC_ALL, "Rus");
-
-    double x = 6.251, y = 0.827, z = 25.001;
-    double b;
-
-    b = pow(y, sqrt(fabs(x))) +
-        pow(cos(y), 3) *
-        (fabs(x - y) * (1 + pow(sin(z), 2) / sqrt(x + y))) /
-        (exp(fabs(x - y)) + x / 2);
-
-    printf("x = %.3f, y = %.3f, z = %.3f\n", x, y, z);
-    printf("b = %.4f\n", b);
-
+    double x = 14.26;
+    double y = -1.22;
+    double z = 3.5e-2
+    double pi = acos(-1.0);
+    double t = (2 * cos(x - pi/6)) / (0.5 + sin(y) * sin(y)) * 
+               (1 + (z * z) / (3 - (z * z) / 5));
+    
+    printf("x = %.2f\n", x);
+    printf("y = %.2f\n", y);
+    printf("z = %.2e\n", z);
+    printf("t = %.6f\n", t);
+    
     return 0;
 }
